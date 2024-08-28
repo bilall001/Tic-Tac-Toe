8b95f827b16cfaafe07f353a8a5871e9c950c2ba
bilall001 <bilalskamboh456@gmail.com>
2024-08-28 21:59:47 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
-import { useState } from "react"
-import Player from "./components/Player.jsx"
-import Gameboard from "./components/Gameboard.jsx"
+import { useState } from "react";
+import Player from "./components/Player.jsx";
+import Gameboard from "./components/Gameboard.jsx";
 function App() {
-  const [activePlayer,setActivePlayer] = useState('X');
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurn, setgameTurn] = useState([]);
+  
+  function handleclicksquare(rowIndex, colIndex) {
+   
+    setActivePlayer((isActive) => (isActive === "X" ? "O" : "X"));
+    setgameTurn(prevTurn => {
+      let currentPlayer = "X";
 
-  function handleclicksquare(){
-    setActivePlayer((isActive) => isActive === 'X' ? 'O': 'X');
+      if (prevTurn.length > 0 && prevTurn[0].player == "X") {
+        currentPlayer = "O";
+      }
+      const newTurn = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurn,
+      ];
+
+      return newTurn;
+    });
   }
+
   return (
     <main>
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActivePlayer={activePlayer === 'X'} />
-        <Player initialName="Player 2" symbol="O" isActivePlayer={activePlayer === 'O'}  />
-      </ol>
-      <Gameboard  isactivesquare={handleclicksquare} activePlayerSymbol={activePlayer}/>
-    </div>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActivePlayer={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActivePlayer={activePlayer === "O"}
+          />
+        </ol>
+        <Gameboard
+          isactivesquare={handleclicksquare}
+          activePlayerSymbol={activePlayer}
+          turns={gameTurn}
+        />
+      </div>
     </main>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,33 +1,42 @@
-import { useState } from "react";
 const initailGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-function Gameboard({ isactivesquare,activePlayerSymbol }) {
-  const [gameBoard, setgameBoard] = useState(initailGameBoard);
+function Gameboard({ isactivesquare, turns }) {
 
-  function handleSymbolchange(rowIndex, colIndex) {
-    setgameBoard((previoussymbol) => {
-      const updatedGameBoard = [
-        ...previoussymbol.map((innerarray) => [...innerarray]),
-      ];
-      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedGameBoard;
-    });
+  let gameBoard = initailGameBoard;
 
-    isactivesquare();
-  }
+  for (const turn of turns){
+
+    const {square, player} = turn;
+    const { row, col} = square;
+    gameBoard[row][col] = player;
+    }
+  // const [gameBoard, setgameBoard] = useState(initailGameBoard);
+
+  // function handleSymbolchange(rowIndex, colIndex) {
+  //   setgameBoard((previoussymbol) => {
+  //     const updatedGameBoard = [
+  //       ...previoussymbol.map((innerarray) => [...innerarray]),
+  //     ];
+  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedGameBoard;
+  //   });
+
+  //   isactivesquare();
+// }
+  
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+       { gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((col, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSymbolchange(rowIndex, colIndex)}>
+                <button onClick={() => isactivesquare(rowIndex,colIndex)}>
                   {col}
                 </button>
               </li>
